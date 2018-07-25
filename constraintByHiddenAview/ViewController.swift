382e56dfb8f5fbb697c5e8b3be3c13d3d3802333
Akash Soni <akashsoni@novomac9s-macbook-pro-2.local>
2018-07-25 23:34:50 +0530

    Initial Commit

---
+++ constraintByHiddenAview/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  constraintByHiddenAview
+//
+//  Created by Akash Soni on 25/07/18.
+//  Copyright © 2018 Akash Soni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var confirmPasswordView: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func confirmTapped(_ sender: Any) {
+        
+    }
+    
+
+}
+
